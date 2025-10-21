3aa7680bd3f58c222d5b4abe8350f7c8f178a616
Carla Puga <84739899+cpugaper@users.noreply.github.com>
2025-10-21 12:09:24 +0200

    Player script in process

--- The Last Key/Assets/Scripts/Player.cs
+++ The Last Key/Assets/Scripts/Player.cs
@@ -2,15 +2,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    // Move in A and D
+    // Space for jump
+    // Left click for attack --> Push other player
+    // If pushed & has key speed increases for 5 seconds
+
+    [SerializeField] private float moveSpeed = 5f;
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        float moveInput = 0f;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveInput = -1f ;
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            moveInput = 1f;
+        }
+
+        if (rb != null)
+        {
+            rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);
+        }
+        else
+        {
+            transform.position += new Vector3(moveInput * moveSpeed * Time.deltaTime, 0, 0);
+        }
     }
 }
